6df883d30eda7b79dd6fd51e2fdd9fa3701bef5b
spyatmycode <akejunifemi11@gmail.com>
2024-02-29 18:33:27 +0100

    fix(): Fixed child process error from html-pdf

--- utils/sendReceipt.js
+++ utils/sendReceipt.js
@@ -30,36 +30,50 @@ export const sendReceipt = async(email, content)=>{
         width: '8in' // A4 width
     }
 
-     pdf.create(await recieptTemplate(content), options).toStream((err, stream) => {
-        if (err) return console.log(err);
-
+    pdf.create(await recieptTemplate(content), {
+        format: 'A4',
+        border: {
+            top: '0',
+            right: '0',
+            bottom: '0',
+            left: '0'
+        },
+        height: '12.5in', // A4 height
+        width: '8in', // A4 width
+        childProcessOptions: {
+            env: {
+                OPENSSL_CONF: '/dev/null',
+            },
+        },
+    }).toStream((err, stream) => {
+        if (err) {
+            console.log("Error creating PDF:", err);
+            return;
+        }
+    
         const mailOptions = {
             from: 'akejunifemi11@gmail.com',
             to: email,
-            subject: 'You bought this tickets from us !',
+            subject: 'You bought this tickets from us!',
             text: 'Please find attached the book ticket PDF.',
             attachments: [
                 {
                     filename: `${content.name}_${content.name}_${content.publishYear}.pdf`,
-                    content: stream
-                }
-            ]
+                    content: stream,
+                },
+            ],
         };
-
+    
         // Send email
-        transporter.sendMail(
-             mailOptions, (error, info) => {
-                 if (error) {
-                    console.log("An error occured while sending your email", error);
-                    
-                 }else{
-                     console.log('Book ticket send successfully:', info);
-                 }
-             }
-
-         )
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log("An error occurred while sending your email:", error);
+            } else {
+                console.log('Book ticket sent successfully:', info);
+            }
+        });
     });
-
+    
 
 
 
